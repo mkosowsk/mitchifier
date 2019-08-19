4bec5de19673be4e842820b4d441ee61a54a92d0
Mitch Kosowski <mitchkosowski@gmail.com>
2019-08-19 19:06:14 -0400

    added copy button

--- src/components/OutputTweet.js
+++ src/components/OutputTweet.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import { Form, Grid, TextArea } from 'semantic-ui-react';
+import { Button, Form, Grid, TextArea } from 'semantic-ui-react';
 
 class OutputTweet extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { copySuccess: '' }
+    }
+
+    copyToClipboard = (e) => {
+        console.log(this);
+        this.textArea.select();
+        document.execCommand('copy');
+        this.setState({ copySuccess: 'Copied!' });
+    };
+
     render() {
         const mitchifiedTweet = this.props.mitchifiedTweet;
 
         return (
-            <Grid padded>
-                <Grid.Row centered>
-                    <Grid.Column width={8}>
-                        <Form>
-                            <TextArea placeholder='Mitchified tweet will go here' value={mitchifiedTweet} />
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+            <div>
+                <Grid padded>
+                    <Grid.Row centered>
+                        <Grid.Column width={8}>
+                            <Form>
+                                <TextArea placeholder='Mitchified tweet will go here' value={mitchifiedTweet} ref={(textarea) => this.textArea = textarea} />
+                            </Form>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Button
+                    color='green'
+                    content='Mitchify™'
+                    onClick={this.copyToClipboard}>Copy
+                </Button>
+            </div>
         )
     }
 }
